93fc34daa95c0fd638c6e90036a8bd8921e74ccb
Begoña Guereca <bguereca@Begonas-MBP-2.lan>
2021-10-14 11:29:40 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OctocatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
